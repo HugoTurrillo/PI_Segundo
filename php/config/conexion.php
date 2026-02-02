18f9ff80e6b84991b3fd50244f29bacc4938101b
Marcos <marcos.2006.gs@gmail.com>
2026-02-02 10:25:53 +0100

    Creacion de base de datos sola

--- php/config/conexion.php
+++ php/config/conexion.php
@@ -1,14 +1,10 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$dbname = "festival_cortos_uem";
+require_once __DIR__ . "/bootstrap_db.php";
 
-
-$conexion = new mysqli($host, $user, $pass, $dbname);
+$conexion = new mysqli("localhost", "root", "", "festival_cortos_uem");
 
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
 }
-$conexion->set_charset("utf8mb4");
 
+$conexion->set_charset("utf8mb4");
